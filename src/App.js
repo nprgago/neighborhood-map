ae69ab8bb1650fbd8ca32f663343de825174820a
ngago <ngago@ubiwhere.pt>
2018-08-06 20:34:21 +0100

    feat: routing and initialize home component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './styles/App.css';
+import React, { Component } from 'react'
+import { Route, Switch } from 'react-router-dom'
+import Home from './components/Home'
+import './styles/App.css'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Switch>
+          <Route exact path='/' component={Home} />
+        </Switch>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
---
+++ src/components/Home.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import '../styles/home.css'
+
+class Home extends Component {
+  
+  state = {
+    isLoading: true
+  }  
+  
+  
+  render () {
+    return (
+      <div className="home"></div>
+    )
+  }
+}
+
+export default Home
